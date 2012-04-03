066c78adf837c88351f34053159721a1d5bc74af
Denny Wu <denny.wu13@gmail.com>
2012-04-03 13:34:28 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<title>Reservasi Hotel</title>
+		<link href="style/site.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<div class="mainContainer">
+			<div class="headerContainer">
+				<div class="headerContain">
+					<div class="logo">LOGO</div>
+					<div class="globalNav">
+						<ul>
+							<li class="linkHome" id="test"><a>Beranda</a></li>
+							<li><a>Lokasi</a></li>
+							<li></li>
+						</ul>
+					</div>
+					<div class="infoHeader">
+						<h1><span>B</span>CC <span>H</span>otel <span>B</span>atam, <span>P</span>enuin</h1>
+						<div class="infoAddressHeader">JL. Makmur Mega Sejahtera Kav. E.1.1 NO.1, Penuin, Batam 21432 Indonesia</div>
+						<div class="infoContact">Phone: (0778) 23459</div>
+					</div>
+				</div>
+				<div class="nav">
+					<ul class="ulNav">
+						<li><a style="padding-right:15px;">Hotel Overview</a></li>
+						<li><a>Rooms</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="contain">
+			</div>
+			<div class="footer">
+			</div>
+		<div>
+	</body>
+</html>
