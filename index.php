192e1e63ef7292d942183b176fa0c23193332d75
Denny Wu <denny.wu13@gmail.com>
2012-04-03 18:05:10 +0700

    slider home

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 	<head>
 		<title>Reservasi Hotel</title>
 		<link href="style/site.css" rel="stylesheet" type="text/css"/>
+		<link href="style/diapo.css" rel="stylesheet" type="text/css" />
+		<script src="script/plugin/jquery.min.js" type="text/javascript"></script>
+		<script src="script/plugin/jquery.easing.1.3.js" type="text/javascript"></script>
+		<script src="script/plugin/jquery.hoverIntent.minified.js" type="text/javascript"></script>
+		<script src="script/plugin/diapo.js" type="text/javascript"></script>
+		<script src="script/index.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<div class="mainContainer">
@@ -29,6 +35,40 @@
 				</div>
 			</div>
 			<div class="contain">
+				<div style="overflow:hidden; width:1220px;margin-top:15px;background-color:#ccc;height:520px;"> 
+					<div class="pix_diapo">
+						<div data-thumb="asset/HomeSlider/thumbs/1.jpg">
+							<img src="asset/HomeSlider/1.jpg">
+							<div class="caption elemHover fromLeft">
+								This is a simple sliding image with caption. You can have more than one caption and decide the layout of the caption via css.
+							</div>
+						</div>	
+						<div data-thumb="asset/HomeSlider/thumbs/2.jpg">
+							<img src="asset/HomeSlider/2.jpg">
+							<div class="caption elemHover fromRight" style="bottom:65px; padding-bottom:5px; color:#ff0; text-transform:uppercase">
+								Here you can see two captions.
+							</div>
+							<div class="caption elemHover fromLeft" style="padding-top:5px;">
+								The first are loaded immediately before than the second one
+							</div>
+						</div>	
+						<div data-thumb="asset/HomeSlider/thumbs/3.jpg">
+							<img src="asset/HomeSlider/3.jpg">
+							<div class="caption elemHover fromLeft">
+								This is a simple sliding image with caption. You can have more than one caption and decide the layout of the caption via css.
+							</div>
+						</div>	
+						<div data-thumb="asset/HomeSlider/thumbs/4.jpg">
+							<img src="asset/HomeSlider/4.jpg"> 
+							<div class="caption elemHover fromRight" style="bottom:65px; padding-bottom:5px; color:#ff0; text-transform:uppercase">
+								Here you can see two captions.
+							</div>
+							<div class="caption elemHover fromLeft" style="padding-top:5px;">
+								The first are loaded immediately before than the second one
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div class="footer">
 			</div>
